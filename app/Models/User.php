0a92f5f9f8ad158cc488280bc0cf3f34b6b44168
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-06-21 13:41:50 +0530

    added authentication using laravel/ui

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function todos(){
+        return $this->hasMany(Todo::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::get('/demo', [App\Http\Controllers\TodoController::class, 'index'])->name('demo');
